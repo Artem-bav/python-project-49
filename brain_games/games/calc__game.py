efc41dde3bd3fd40727955d072da4956569b7132
Artem-bav <tigra_bav@mail.ru>
2023-01-20 10:27:03 +0300

    добавил calc__game yначал дробить модуль

---
+++ brain_games/games/calc__game.py
@@ -0,0 +1,43 @@
+def calc__user():
+    import prompt  # ожидание ввода
+    import random  # случайный выбор
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'''Hello, {name}!
+What is the result of the expression?''')
+						#    counter_i = 0  # счетчик правильных ответов
+						#    while counter_i <= 2:
+    signs = "*+-"
+    dif_signs = ''
+        # answer = 0
+    answer = 0
+    dif_signs = random.choice(signs)  # случайный выбор знака
+    a = random.randint(0, 100)
+    print(a)
+    b = random.randint(0, 100)
+    print(b)
+    if dif_signs == '+':
+        c = a + b
+    elif dif_signs == '-':
+        c = a - b
+    else:
+        c = a * b
+    print(f'{dif_signs}')
+    print(c)
+    return c
+        
+        
+#        print(f'Question: {a} {dif_signs} {b} , (  :) {c})')
+#        answer = int(prompt.string('Your answer: '))  # перем->ответ
+#        if c == answer:
+#            print('Correct!')
+#            counter_i += 1
+#        else:
+#            print(f"""Question: {a} {dif_signs} {b}
+#Your answer: {answer}
+#'{answer}' is wrong answer ;(. Correct answer was '{c}'.
+#Let's try again, {name}!""")
+#            # i = 4
+#            break
+#        if counter_i == 3:
+#            print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain__calc.py
@@ -0,0 +1,7 @@
+def main():
+    from brain_games.games.calc__game import calc__user
+    calc__user()
+
+
+if __name__ == '__main__':
+    main()
